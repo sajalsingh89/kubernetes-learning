e8c304490dcfbbc3f845d9fdc89b245a35f790b5
sajalsingh89 <sajalsingh587@gmail.com>
2025-08-24 23:57:36 -0500

    test

--- ingress-resource.sh
+++ ingress-resource.sh
@@ -2,3 +2,5 @@ kubectl describe ingress demo-ingress
 kubectl get ingress
 kubectl create ingress --help
 kubectl create ingress main-ingress --rule="example.internal/*=example-service:80" --rule="perilious.internal/*=perlious-service:80"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.0/deploy/static/provider/cloud/deploy.yaml
+kubectl expose pod example-pod --name example-service --port=80 --target-port=80
