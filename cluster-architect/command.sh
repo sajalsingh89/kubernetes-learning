6bf78d7506b85ffc75d9ec72a9105b3f0a3f191e
sajalsingh89 <sajalsingh587@gmail.com>
2025-09-13 17:54:22 -0500

    test

--- cluster-architect/command.sh
+++ cluster-architect/command.sh
@@ -3,18 +3,6 @@ containerd config default | sudo tee /etc/containerd/config.toml
 sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
 
-# Or create a basic config manually
-sudo tee /etc/containerd/config.toml <<EOF
-version = 2
-[plugins."io.containerd.grpc.v1.cri"]
-  [plugins."io.containerd.grpc.v1.cri".containerd]
-    [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
-      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
-        runtime_type = "io.containerd.runc.v2"
-        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
-          SystemdCgroup = true
-EOF
-
 # Update package list
 sudo apt update
 
