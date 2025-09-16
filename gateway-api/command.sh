1c8de4a9ee0ad3a57f29d594abc87f24c961f55d
sajalsingh89 <sajalsingh587@gmail.com>
2025-09-15 20:22:52 -0500

    gateway api

--- gateway-api/command.sh
+++ gateway-api/command.sh
@@ -4,5 +4,14 @@ kubectl apply -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v1
 kubectl get namespace
 kubectl get pods -n nginx-gateway
 kubectl get service -n nginx-gateway
+kubectl apply -f .\gateway-api\create-gateway.yaml
 kubectl create deployment apache --image=httpd:latest --port=80
 kubectl expose deployment apache --name=apache-service --port=80 --target-port=80 --type=ClusterIP
+kubectl apply -f .\gateway-api\http-route.yaml
+
+########## Delete after Creating it
+kubectl delete pods --all
+kubectl delete svc apache-service
+kubectl delete gateway nginx-gateway
+kubectl delete -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v1.6.2/deploy/default/deploy.yaml
+kubectl delete -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v1.6.2/deploy/crds.yaml
